5eb00e6591f16730121d1552dd48732b0f21b6a4
david Huang AMP <david_huang@amp.com.au>
2023-03-13 06:25:20 +1100

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-
+import { ApiserviceComponent } from 'projects/apiservice/src/public-api';
+import {ApiserviceService} from 'projects/apiservice/src/lib/apiservice.service'
+import { Customer } from 'projects/apiservice/src/lib/customerModel';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'apicliz';
+
+ cust:any[]=[];
+
+  constructor(public apicall:ApiserviceService){
+  console.log(this.apicall.getcustomer());
+
+  this.apicall.getcustomer().forEach(lst=>{
+    this.cust.push(lst);
+  });
+
+   
+ 
+  }
+
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {HttpClient, HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ApiserviceModule } from "../../projects/apiservice/src/lib/apiservice.module";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    providers: [HttpClientModule],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        ApiserviceModule,
+        HttpClientModule
+    ]
 })
-export class AppModule { }
+export class AppModule { 
+  
+}
